dd429ceee1db6caab28297aef19c4648515f7ac2
Dakshina Scott <dakshina.scott@gmail.com>
2017-12-13 14:44:51 +0000

    can select answers

--- app/components/Quiz.jsx
+++ app/components/Quiz.jsx
@@ -19,13 +19,20 @@ QuestionHeader.propTypes = {
   countryName: PropTypes.string.isRequired,
 };
 
-function SelectAnswer({ options }) {
+function SelectAnswer({ options, userAnswer, onSelect }) {
   return (
     <div className="select-answer">
       {options.map(option =>
           (
             <label htmlFor={option} key={option}>
-              <input type="radio" id={option} name="answerOption" value={option} />
+              <input
+                type="radio"
+                id={option}
+                name="answerOption"
+                value={option}
+                checked={option === userAnswer}
+                onChange={onSelect}
+              />
               {option}
             </label>
           ))}
@@ -35,13 +42,15 @@ function SelectAnswer({ options }) {
 
 SelectAnswer.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  userAnswer: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
 };
 
-function QuestionNavigation({ questionNumber }) {
+function QuestionNavigation({ questionNumber, onNext, onPrevious }) {
   return (
     <nav>
-      { questionNumber.current !== 0 ? <button>Previous</button> : null }
-      { questionNumber.current !== questionNumber.total - 1 ? <button>Next</button> : null }
+      { questionNumber.current !== 0 ? <button onClick={onPrevious}>Previous</button> : null }
+      { questionNumber.current !== questionNumber.total - 1 ? <button onClick={onNext}>Next</button> : null }
       { questionNumber.current === questionNumber.total - 1 ? <button>Submit</button> : null }
     </nav>
   );
@@ -52,9 +61,11 @@ QuestionNavigation.propTypes = {
     number: PropTypes.number,
     total: PropTypes.number,
   }).isRequired,
+  onNext: PropTypes.func.isRequired,
+  onPrevious: PropTypes.func.isRequired,
 };
 
-function Question({ question, questionNumber }) {
+function Question({ question, userAnswer, questionNumber, onSelect }) {
   return (
     <fieldset>
       <legend>
@@ -65,6 +76,8 @@ function Question({ question, questionNumber }) {
       </legend>
       <SelectAnswer
         options={question.options}
+        userAnswer={userAnswer}
+        onSelect={onSelect}
       />
     </fieldset>
   );
@@ -76,24 +89,73 @@ Question.propTypes = {
     number: PropTypes.number,
     total: PropTypes.number,
   }).isRequired,
+  onSelect: PropTypes.func.isRequired,
+  userAnswer: PropTypes.string.isRequired,
 };
 
-function Quiz() {
-  const currentQuestion = 4;
-  return (
-    <main>
-      <h1>Capital Cities Quiz</h1>
-      <form>
-        <Question
-          question={questions[currentQuestion]}
-          questionNumber={{ current: currentQuestion, total: questions.length }}
-        />
-        <QuestionNavigation
-          questionNumber={{ current: currentQuestion, total: questions.length }}
-        />
-      </form>
-    </main>
-  );
+class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentQuestion: 0,
+      userAnswers: [],
+    };
+
+    this.handleNext = this.handleNext.bind(this);
+    this.handlePrevious = this.handlePrevious.bind(this);
+    this.handleSelection = this.handleSelection.bind(this);
+  }
+
+  handleNext(event) {
+    event.preventDefault();
+    this.setState(prevState =>
+      ({
+        currentQuestion: prevState.currentQuestion + 1,
+      }));
+  }
+
+  handlePrevious(event) {
+    event.preventDefault();
+    this.setState(prevState =>
+      ({
+        currentQuestion: prevState.currentQuestion - 1,
+      }));
+  }
+
+  handleSelection(event) {
+    const newAnswer = event.target.value;
+    this.setState((prevState) => {
+      const newAnswers = [...prevState.userAnswers];
+      newAnswers[prevState.currentQuestion] = newAnswer;
+      return ({
+        userAnswers: newAnswers,
+      });
+    });
+  }
+
+  render() {
+    const { currentQuestion, userAnswers } = this.state;
+
+    return (
+      <main>
+        <h1>Capital Cities Quiz</h1>
+        <form>
+          <Question
+            question={questions[currentQuestion]}
+            questionNumber={{ current: currentQuestion, total: questions.length }}
+            onSelect={this.handleSelection}
+            userAnswer={userAnswers[currentQuestion] || ''}
+          />
+          <QuestionNavigation
+            questionNumber={{ current: currentQuestion, total: questions.length }}
+            onNext={this.handleNext}
+            onPrevious={this.handlePrevious}
+          />
+        </form>
+      </main>
+    );
+  }
 }
 
 module.exports = Quiz;
